b4f741a1222b63bcf04c8b00ceb0551d41446bce
Aleks <bla3e316@gmail.com>
2017-11-20 21:07:30 +0200

    Interval Button State Props Message

--- index.vr.js
+++ index.vr.js
@@ -4,8 +4,21 @@ import {
   asset,
   Pano,
   Text,
-  View
+  View,
+  VrButton
 } from 'react-vr';
+
+export default class WelcomeToVR extends React.Component {
+
+    render() {
+        return (
+            <View>
+                <Pano source={asset('222.jpg')}/>
+                <NestedMessage message={"Default Message"}/>
+            </View>
+        );
+    }
+};
 
 class  NestedMessage extends React.Component {
 
@@ -17,47 +30,41 @@ class  NestedMessage extends React.Component {
         },6000);
     }
 
+    handleClick () {
+        this.setState ({message: "Button Up Message"});
+    }
+
     render() {
         return (
-            <Text
-                style={{
-                    backgroundColor: 'rgba(0,0,0,0.4)',
-                    fontSize: 0.8,
-                    color: '#bada55',
-                    fontWeight: '400',
-                    layoutOrigin: [0.5, 0.5],
-                    paddingLeft: 0.2,
-                    paddingRight: 0.2,
-                    textAlign: 'center',
-                    textAlignVertical: 'center',
-                    transform: [
-                        {translate: [0, 0.2, -5]},
-                        //{rotateY: this.state.rotation},
-                        {rotateX: 20},
-                        {rotateZ: -10}
-                    ],
-                }}
-            >
-                {this.state.message}
-                {/*{this.props.message}*/}
-            </Text>
-
+            <View>
+                <VrButton
+                onClick={this.handleClick.bind(this)}
+                >
+                    <Text
+                        style={{
+                            backgroundColor: 'rgba(0,0,0,0.4)',
+                            fontSize: 0.8,
+                            color: '#bada55',
+                            fontWeight: '400',
+                            layoutOrigin: [0.5, 0.5],
+                            paddingLeft: 0.2,
+                            paddingRight: 0.2,
+                            textAlign: 'center',
+                            textAlignVertical: 'center',
+                            transform: [
+                                {translate: [0, 0.2, -5]},
+                                //{rotateY: this.state.rotation},
+                                {rotateX: 20},
+                                {rotateZ: -10}
+                            ],
+                        }}>
+                        {this.state.message}
+                        , {this.props.message}
+                    </Text>
+                </VrButton>
+            </View>
         );
     }
-};
-
-
-export default class WelcomeToVR extends React.Component {
-
-
-  render() {
-    return (
-      <View>
-        <Pano source={asset('222.jpg')}/>
-        <NestedMessage message={"Hello NM VR"}/>
-      </View>
-    );
-  }
 };
 
 AppRegistry.registerComponent('WelcomeToVR', () => WelcomeToVR);
