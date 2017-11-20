2b8e545b0463fcffa811f6cf13d4401714f347f0
Aleks <bla3e316@gmail.com>
2017-11-20 19:58:40 +0200

    props Message

--- index.vr.js
+++ index.vr.js
@@ -7,46 +7,70 @@ import {
   View
 } from 'react-vr';
 
+class  NestedMessage extends React.Component {
+
+    render() {
+        return (
+            <Text
+                style={{
+                    backgroundColor: 'rgba(0,0,0,0.4)',
+                    fontSize: 0.8,
+                    color: '#bada55',
+                    fontWeight: '400',
+                    layoutOrigin: [0.5, 0.5],
+                    paddingLeft: 0.2,
+                    paddingRight: 0.2,
+                    textAlign: 'center',
+                    textAlignVertical: 'center',
+                    transform: [
+                        {translate: [0, 0.2, -3.1]},
+                        //{rotateY: this.state.rotation},
+                        {rotateX: 20},
+                        {rotateZ: -10}
+                    ],
+                }}
+            >
+                {this.props.message}
+            </Text>
+
+        );
+    }
+};
+
 export default class WelcomeToVR extends React.Component {
     constructor(props) {
         super(props);
     }
 
+
   render() {
     return (
       <View>
-        <Pano source={asset('111.jpg')}/>
-        <Text
-          style={{
-            backgroundColor: 'rgba(0,0,0,0.4)',
-            fontSize: 0.8,
-              color: '#bada55',
-            fontWeight: '500',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [
-                {translate: [0, 0.2, -3.1]},
-                //{rotateY: this.state.rotation},
-                {rotateX: 20},
-                {rotateZ: -10}
-            ],
-          }}
-            >
-          VR Task
-        </Text>
+        <Pano source={asset('222.jpg')}/>
+        <NestedMessage message={"Hello NM VR"}/>
 
-
-
-        {/*<Text style={front}>*/}
-            {/*Back*/}
+        {/*<Text*/}
+          {/*style={{*/}
+            {/*backgroundColor: 'rgba(0,0,0,0.4)',*/}
+            {/*fontSize: 0.8,*/}
+              {/*color: '#bada55',*/}
+            {/*fontWeight: '400',*/}
+            {/*layoutOrigin: [0.5, 0.5],*/}
+            {/*paddingLeft: 0.2,*/}
+            {/*paddingRight: 0.2,*/}
+            {/*textAlign: 'center',*/}
+            {/*textAlignVertical: 'center',*/}
+            {/*transform: [*/}
+                {/*{translate: [0, 0.2, -3.1]},*/}
+                {/*//{rotateY: this.state.rotation},*/}
+                {/*{rotateX: 20},*/}
+                {/*{rotateZ: -10}*/}
+            {/*],*/}
+          {/*}}*/}
+            {/*>*/}
+            {/*{this.props.message}*/}
         {/*</Text>*/}
 
-          {/*<Text style={back}>*/}
-              {/*Front*/}
-          {/*</Text>*/}
 
       </View>
     );
